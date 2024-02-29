97aae0490cb494f4b22dbf0730be4e9cfb817004
CarolinaSilva <142807604+CarolinaSilva22@users.noreply.github.com>
2024-02-29 11:09:15 -0300

    Update Main.java

--- teste.java/src/Main.java
+++ teste.java/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-              int num1 = 3, num2 =5, soma;
+        int num1 = 3, num2 =5, soma;
 
         soma = num1 + num2;
+        system.out.println(soma);
     }
 }
